d8b44a3392876baa9b09cdb3dab2c431b7e8ff96
Nate Lovell <Natelovell99@gmail.com>
2020-09-03 02:31:45 -0600

    Create Collapsable UI Element.jsx

---
+++ Collapsable UI Element.jsx
@@ -0,0 +1,27 @@
+﻿var window = new Window("palette", "Collapsable UI Element", undefined);
+window.orientation = "column";
+var groupOne = window.add("group", undefined, "groupOne");
+groupOne.orientation = "column";
+var button = groupOne.add("button", undefined, "Collapse!");
+var textOne = groupOne.add("statictext", undefined, "Some sample text 1");
+var textTwo = groupOne.add("statictext", undefined, "Some sample text 2");
+
+var collapsed = false;
+
+button.onClick = function() {
+        if(collapsed) {
+            groupOne.size = [102, 73];
+            window.size = [132, 103];
+            collapsed = false;
+            } else {
+            groupOne.size = [102, 25];  
+            window.size = [132, 50];
+            collapsed = true;
+                }
+    }
+
+window.center();
+window.show();
+
+$.writeln(window.size);
+//$.writeln(groupOne.size);
